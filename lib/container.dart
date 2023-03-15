51f531a3ba6a2ce65d5acb0839b6e637fa959db2
eeimaan <emanyaqoob379@gmail.com>
2023-03-16 00:36:38 +0500

    new widgets

---
+++ lib/container.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter App with Containers',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Flutter App with Containers'),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              Container(
+                width: 100,
+                height: 100,
+                color: Colors.red,
+              ),
+              Container(
+                width: 150,
+                height: 150,
+                color: Colors.green,
+              ),
+              Container(
+                width: 200,
+                height: 200,
+                color: Colors.blue,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
